7acbcbae50ae09bebeb9bba998e751d860bde736
Brendan Benner <brendanbenner@berkeley.edu>
2020-09-02 17:41:54 -0700

    Updates

---
+++ precache-manifest.e6e71ca7f1f909794a5ff36aa5217023.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d5da39dcbe8a7397701307c0edb67b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62b2be6d3013e05a7fd1",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "42f768956bdeb4f2f102",
+    "url": "/static/css/main.c975563e.chunk.css"
+  },
+  {
+    "revision": "62b2be6d3013e05a7fd1",
+    "url": "/static/js/2.33a7b3bf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.33a7b3bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f768956bdeb4f2f102",
+    "url": "/static/js/main.a5b557f3.chunk.js"
+  },
+  {
+    "revision": "1d174dd0b0ae47ee00c0",
+    "url": "/static/js/runtime-main.60707d35.js"
+  },
+  {
+    "revision": "f368e509ae0f2ed9398b365f6894ec19",
+    "url": "/static/media/Resume.f368e509.pdf"
+  },
+  {
+    "revision": "b992b19aa2066cefa24f88dd2724bb7a",
+    "url": "/static/media/click.b992b19a.png"
+  },
+  {
+    "revision": "7c08b9adb87aaf398cb08067a33cf680",
+    "url": "/static/media/ecodatalab.7c08b9ad.png"
+  },
+  {
+    "revision": "ca5f6add95ab4a3c36f719098e73d8d6",
+    "url": "/static/media/me.ca5f6add.jpg"
+  },
+  {
+    "revision": "9ad9285fa9718e0071fddc2dd3843de0",
+    "url": "/static/media/storm2.9ad9285f.png"
+  },
+  {
+    "revision": "a27e56fc43539e09537b1a47797107c8",
+    "url": "/static/media/textsummarizer.a27e56fc.png"
+  }
+]);
